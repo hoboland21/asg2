34c6e056f5451a7074a7a134038de0d38d7d01c4
hoboland21 <jc@saipantech.com>
2023-08-04 11:59:07 -0700

    start

--- project_data/angular/startup.sh
+++ project_data/angular/startup.sh
@@ -1,8 +1,8 @@
 
-if [ ! -d asg ] ; then 
-ng new asg --routing --defaults=true
+if [ ! -d asg2 ] ; then 
+ng new asg2 --routing --defaults=true
 fi
-cd asg
+cd asg2
 
 
 #ng build --prod --output-path /usr/src/app/django/survey/static/ang/main  --watch --output-hashing none
