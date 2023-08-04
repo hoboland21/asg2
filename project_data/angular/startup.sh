ef27a0b1b220c3e0fc267315598c73958fb5e63b
JC Clark <jc@saipantech.com>
2023-08-04 11:46:07 -0700

    stuff

---
+++ project_data/angular/startup.sh
@@ -0,0 +1,10 @@
+
+if [ ! -d asg ] ; then 
+ng new asg --routing --defaults=true
+fi
+cd asg
+
+
+#ng build --prod --output-path /usr/src/app/django/survey/static/ang/main  --watch --output-hashing none
+
+ng serve --host 0.0.0.0 --disable-host-check
