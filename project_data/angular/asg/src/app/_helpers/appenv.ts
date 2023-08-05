bda2fd668ba6130c23fef596c8b70cf3c05f8fb7
JC Clark <jc@saipantech.com>
2023-08-05 02:21:45 -0700

    added material

---
+++ project_data/angular/asg/src/app/_helpers/appenv.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+Injectable()
+export class AppEnv{
+    public API_URL = 'http://rh-tst-03:4200'
+    public WEB_API = 'http://rh-tst-03:4200/webapi'
+}
+
