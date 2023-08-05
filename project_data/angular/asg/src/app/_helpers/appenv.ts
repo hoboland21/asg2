bc331583fa4f4b672298c62075cf7c0510a7441f
JC Clark <jc@saipantech.com>
2023-08-05 14:52:17 -0700

    working angular

--- project_data/angular/asg/src/app/_helpers/appenv.ts
+++ project_data/angular/asg/src/app/_helpers/appenv.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 Injectable()
 export class AppEnv{
-    public API_URL = 'http://rh-tst-03:4200'
-    public WEB_API = 'http://rh-tst-03:4200/webapi'
+    public API_URL = 'http://rh-tst-02:4200'
+    public WEB_API = 'http://rh-tst-02:4200/webapi'
 }
 
