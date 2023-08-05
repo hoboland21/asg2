7e449cc3f07a27d106b4aff9b0c6f0bac4773f19
hoboland21 <jc@saipantech.com>
2023-08-05 00:31:24 -0700

    Cleaned

--- project_data/angular/startup.sh
+++ project_data/angular/startup.sh
@@ -1,8 +1,8 @@
 
 if [ ! -d asg2 ] ; then 
-ng new asg2 --routing --defaults=true
+ng new asg --routing --defaults=true
 fi
-cd asg2
+cd asg
 npm install
 #ng build --prod --output-path /usr/src/app/django/survey/static/ang/main  --watch --output-hashing none
 ng serve --host 0.0.0.0 --disable-host-check
