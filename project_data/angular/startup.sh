84dfb49d0475f080da1c5cc4b47c114f569572d7
hoboland21 <jc@saipantech.com>
2023-08-06 20:56:29 -0700

    sdf

--- project_data/angular/startup.sh
+++ project_data/angular/startup.sh
@@ -4,5 +4,5 @@ ng new asg --routing --defaults=true
 fi
 cd asg
 npm install
-ng build --prod --output-path /usr/src/app/nginx/main  --watch --output-hashing none
+ng build --output-path /usr/src/app/nginx/main  --watch --output-hashing none
 ng serve --host 0.0.0.0 --disable-host-check
